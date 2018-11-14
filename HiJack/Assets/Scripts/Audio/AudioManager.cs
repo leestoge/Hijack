d6ffee35813149caa08a5a9c09286cf1161b48ef
Lee <L918Sturrock@hotmail.com>
2018-11-14 00:54:16 +0000

    Created & configured audio manager script, plays game ambience on main menu load

---
+++ HiJack/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+	// Use this for initialization
+	void Awake()
+	{
+	    if (instance == null)
+	    {
+	        instance = this; // do not duplicate the audiomanager (and therefore the audio)
+	    }
+	    else
+	    {
+	        Destroy(gameObject); // destroy if duplicate
+	        return;
+	    }
+        DontDestroyOnLoad(gameObject); // keep audiomanager persistant (one audiomanager to deal with all our scenes rather than multiple)
+
+	    foreach (var s in sounds)
+	    {
+	        s.source = gameObject.AddComponent<AudioSource>();
+	        s.source.clip = s.clip;
+
+	        s.source.volume = s.volume;
+	        s.source.pitch = s.pitch;
+	        s.source.loop = s.loop;
+	    }
+	}
+
+    void Start()
+    {
+        Play("Ambience"); // Play ambience on game start
+    }
+	
+	// Update is called once per frame
+	public void Play(string name)
+	{
+	   var s = Array.Find(sounds, sound => sound.name == name);
+
+	    if (s == null)
+	    {
+            Debug.LogWarning("Sound: " + name + " not found!");
+	        return;
+	    }
+
+       s.source.Play();
+	}
+}
