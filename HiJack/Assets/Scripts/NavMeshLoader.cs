7e0d963bddcb72df7a23193fc873f27545a6a41a
Lee <L918Sturrock@hotmail.com>
2018-11-16 12:13:58 +0000

    Minor formatting changes

--- HiJack/Assets/Scripts/NavMeshLoader.cs
+++ HiJack/Assets/Scripts/NavMeshLoader.cs
@@ -3,10 +3,8 @@ using UnityEngine.AI;
 
 public class NavMeshLoader : MonoBehaviour
 {
-
     public NavMeshSurface surface;
 
-    // Use this for initialization
     void Start()
     {
         // UPDATE NAVMESH
