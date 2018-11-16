474c30bad4fbcf884f6014de1fd4892277790872
Lee <L918Sturrock@hotmail.com>
2018-11-16 12:12:04 +0000

    Created NavMeshLoader script that dynamically generates the NavMesh on game start and applied it to the arena model.

---
+++ HiJack/Assets/Scripts/NavMeshLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshLoader : MonoBehaviour
+{
+
+    public NavMeshSurface surface;
+
+    // Use this for initialization
+    void Start()
+    {
+        // UPDATE NAVMESH
+        surface.BuildNavMesh();
+        Debug.Log("NAVMESH UPDATED.");
+    }
+}
