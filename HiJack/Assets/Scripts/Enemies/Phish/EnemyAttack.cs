0e2ad4b5994ef431bce2a639977990245980f4ca
GodFatherCMB <43375989+GodFatherCMB@users.noreply.github.com>
2018-11-17 22:37:46 +0000

    player health for ui
    contains
    enemy attack
    player health

---
+++ HiJack/Assets/Scripts/Enemies/Phish/EnemyAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    private Animator _animator;
+    private GameObject _player;
+    private bool _collidedWithPlayer;
+
+    void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool("IsNearPlayer", true);
+        }
+        print("enter trigger with _player");
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject == _player)
+        {
+            _collidedWithPlayer = true;
+        }
+        print("enter collided with _player");
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject == _player)
+        {
+            _collidedWithPlayer = false;
+        }
+        print("exit collided with _player");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool("IsNearPlayer", false);
+        }
+        print("exit trigger with _player");
+    }
+
+    private void Attack()
+    {
+        if (_collidedWithPlayer)
+        {
+            _player.GetComponent<PlayerHealth>().TakeDamage(10);
+        }
+    }
+}
---
+++ HiJack/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider HealthBar;
+    public float Health = 100;
+
+    private float _currentHealth;
+
+    void Start()
+    {
+        _currentHealth = Health;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        HealthBar.value = _currentHealth;
+    }
+}
