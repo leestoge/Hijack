c931a1e081f63f40ea1c23d78d527a28ca91c467
Lee <L918Sturrock@hotmail.com>
2018-11-19 02:20:51 +0000

    Reworked melee so the player can hold mouse1 down to continually slash, added a sound file for slashing

--- HiJack/Assets/Scripts/Player/Weapon/QuantumKnife.cs
+++ HiJack/Assets/Scripts/Player/Weapon/QuantumKnife.cs
@@ -3,36 +3,37 @@
 public class QuantumKnife : MonoBehaviour
 {
     public Camera fpsCam;
-    public float distance;
+    public float distance; // 5 is good
+    public int damage; // 10 ?
     public LayerMask myLayerMask;
-    public int damage;
 
-	// Use this for initialization
-	void Start ()
+    public float swingRate = 1.3f;
+    private float nextTimeToSwing;
+
+    // Update is called once per frame
+    void Update ()
 	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    if (Input.GetButtonDown("Fire1"))
+	    if (Input.GetButton("Fire1") && Time.time >= nextTimeToSwing)
 	    {
-	        Slash();
+	        nextTimeToSwing = Time.time + 1f / swingRate;
+	        FindObjectOfType<AudioManager>().Play("SwordSwing");
+            Slash();
 	    }
 	}
 
     void Slash()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, distance, myLayerMask))
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, distance, myLayerMask))
         {
-            targetPhish target = hit.transform.GetComponent<targetPhish>();
+            Debug.Log(hitInfo.transform.name);
+
+            targetPhish target = hitInfo.transform.GetComponent<targetPhish>();
             if (target != null)
             {
-                Debug.Log("Enemy hit");
+                target.TakeDamage(damage);
             }
-            Debug.Log(hit.transform.name);
+
         }
     }
 }
