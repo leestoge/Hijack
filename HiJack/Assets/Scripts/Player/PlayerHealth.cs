bc20c7fec5e2cd58d927a3078d37dfab83ca6e56
Lee <L918Sturrock@hotmail.com>
2018-11-25 04:45:52 +0000

    Added player damaged sounds and got the slider working (Charles used wrong namespace)

--- HiJack/Assets/Scripts/Player/PlayerHealth.cs
+++ HiJack/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
-using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public Slider HealthBar;
-    public float Health = 100f;
+    public float maxHealth = 100f;
 
     private float _currentHealth;
 
     void Start()
     {
-        _currentHealth = Health;
+        _currentHealth = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
+        var damageSounds = new[]
+        {
+            "Damage1",
+            "Damage2",
+            "Damage3",
+            "Damage4"
+        }; // array of strings that holds the damage sound names
+        System.Random rnd = new System.Random();
+        int range = rnd.Next(0, damageSounds.Length);
+        string soundToPlay = damageSounds[range];
+
+        FindObjectOfType<AudioManager>().Play(soundToPlay);
+
         _currentHealth -= damage;
         HealthBar.value = _currentHealth;
     }
