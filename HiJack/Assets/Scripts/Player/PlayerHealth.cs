c06e53a1d408a94cf46f6f0568ee399f8fb40c89
Lee <L918Sturrock@hotmail.com>
2018-11-25 03:26:07 +0000

    added "f" as it is a float.

--- HiJack/Assets/Scripts/Player/PlayerHealth.cs
+++ HiJack/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine.Experimental.UIElements;
 public class PlayerHealth : MonoBehaviour
 {
     public Slider HealthBar;
-    public float Health = 100;
+    public float Health = 100f;
 
     private float _currentHealth;
 
