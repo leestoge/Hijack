b5c47f5fe819506b2767da8871e996fab93cab96
Calum <43376276+Calum532@users.noreply.github.com>
2018-11-26 18:59:03 +0000

    Attempted to stop camera from moving in pause menu

--- HiJack/Assets/Scripts/PauseMenu.cs
+++ HiJack/Assets/Scripts/PauseMenu.cs
@@ -6,6 +6,10 @@ public class PauseMenu : MonoBehaviour {
 
     public GameObject pauseMenuUI;
     public GameObject GameMenuHUD;
+    /* public float xMouseSensitivity = 50.0f;
+    public float yMouseSensitivity = 50.0f;
+    private float rotX;
+    private float rotY; */
 
     // Update is called once per frame
     void Update ()
@@ -30,6 +34,10 @@ public class PauseMenu : MonoBehaviour {
         Time.timeScale = 1f;
         GameIsPaused = false;
         Cursor.visible = false;
+        /* xMouseSensitivity = 50.0f;
+        yMouseSensitivity = 50.0f;
+        rotX -= Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * 0.02f;
+        rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0.02f; */
     }
 
     void Pause()
@@ -39,5 +47,9 @@ public class PauseMenu : MonoBehaviour {
         Time.timeScale = 0f;
         GameIsPaused = true;
         Cursor.visible = true;
+        /* xMouseSensitivity = 0f;
+        yMouseSensitivity = 0f;
+        rotX -= Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * 0f;
+        rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0f; */
     }
 }
