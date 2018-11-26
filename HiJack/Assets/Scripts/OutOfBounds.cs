dc7efba51065b6a55effad526e4a21f0fb39bd02
Lee <L918Sturrock@hotmail.com>
2018-11-26 17:16:53 +0000

    Added "out of bounds" funtionality, that'll return the player back to the map if they fall off

---
+++ HiJack/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // J.A.C.K sound clip?
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
