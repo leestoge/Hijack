eb1b6bbb293580e68f5d450f2aaf57cb1a09acd0
Lee <L918Sturrock@hotmail.com>
2018-11-26 16:26:04 +0000

    Removed debugging stuff

--- HiJack/Assets/Scripts/Enemies/Phish/EnemyAttack.cs
+++ HiJack/Assets/Scripts/Enemies/Phish/EnemyAttack.cs
@@ -2,39 +2,29 @@
 
 public class EnemyAttack : MonoBehaviour
 {
-    private GameObject _player;
-    private bool _collidedWithPlayer;
+    private GameObject player;
 
     void Awake()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == _player)
+        if (other.gameObject == player)
         {
             print("enter trigger with _player");
            Attack();
             print("attempting to do damage");
-        }    
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == _player)
-        {
-            print("exit trigger with _player");
-            _collidedWithPlayer = false;
-        }      
+        }
     }
 
     private void Attack()
     {
-        PlayerHealth playerDamage = _player.GetComponent<PlayerHealth>();
+        PlayerHealth playerDamage = player.GetComponent<PlayerHealth>();
         if (playerDamage != null)
         {
-            playerDamage.TakeDamage(10f);
+            playerDamage.TakeDamage(5f);
         }
     }
 }
