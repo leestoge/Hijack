08fe0d9f83e03970d7ca91893db50fcc3924f935
Calum <43376276+Calum532@users.noreply.github.com>
2018-11-26 18:26:39 +0000

    Started work on a Pause Menu, Increased mouse sensitivity

---
+++ HiJack/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject GameMenuHUD;
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        GameMenuHUD.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        GameMenuHUD.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true;
+    }
+}
