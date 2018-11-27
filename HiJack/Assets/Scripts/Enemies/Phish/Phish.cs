c51db0f84155b78ec31574149ace73bc50820cf1
Lee <L918Sturrock@hotmail.com>
2018-11-27 02:15:40 +0000

    Added randomization to the drop chance of pickups, toggled hitbox during dying state to avoid instantiating unecessary pickups.

--- HiJack/Assets/Scripts/Enemies/Phish/Phish.cs
+++ HiJack/Assets/Scripts/Enemies/Phish/Phish.cs
@@ -5,17 +5,21 @@ public class Phish : MonoBehaviour
 {
     private GameObject player;
     private NavMeshAgent enemy;
+    private BoxCollider hitBox;
 
     public float health = 50f;
     public ParticleSystem deathParticles;
     public GameObject bluePickup;
+    System.Random rnd = new System.Random();
+    
 
     // Use this for initialization
     void Start ()
 	{
 	    player = GameObject.FindGameObjectWithTag("Player"); // find player
-	    enemy = GetComponent<NavMeshAgent>(); // initialise nav mesh
-	}
+	    enemy = GetComponent<NavMeshAgent>(); // get navmesh component from the game object
+	    hitBox = GetComponent<BoxCollider>();// get box collider component from the game object
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -34,9 +38,19 @@ public class Phish : MonoBehaviour
 
     void Die()
     {
+        hitBox.enabled = false;
         enemy.Stop();
-        Instantiate(bluePickup, transform.localPosition + (transform.transform.up * 1), Quaternion.identity);
+        DropPickup();
         deathParticles.Play(); // play particle
         Destroy(gameObject, 0.5f); // delay destroy by 1 second
+    }
+
+    void DropPickup()
+    {        
+        int dropChance = rnd.Next(1, 3);
+        if (dropChance != 1) // if "drop chance" is not equal to 1 then proceed
+        {
+            Instantiate(bluePickup, transform.localPosition + (transform.transform.up * 1), Quaternion.identity);
+        }
     }
 }
