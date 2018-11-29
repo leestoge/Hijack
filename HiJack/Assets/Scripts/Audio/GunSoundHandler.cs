3d022573fff5db6db4700be2c2fc0605468ce4a8
Lee <L918Sturrock@hotmail.com>
2018-11-29 03:37:05 +0000

    Set up GunSoundHandler.cs script to manage the minigun fire sounds & destroy them periodically.
    Destroying them will help game performance

---
+++ HiJack/Assets/Scripts/Audio/GunSoundHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GunSoundHandler : MonoBehaviour 
+{
+
+#region Variables
+    private float totalTimeBeforeDestroy;
+#endregion
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var sound = this.GetComponent<AudioSource>();
+	    totalTimeBeforeDestroy = sound.clip.length;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    totalTimeBeforeDestroy -= Time.deltaTime;
+
+	    if (totalTimeBeforeDestroy <= 0f)
+	    {
+            Destroy(this.gameObject);
+	    }
+	}
+}
