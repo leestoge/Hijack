0c4e8d91835d57bcda36e7202aab4eb194f41fdf
Lee <L918Sturrock@hotmail.com>
2019-10-11 00:17:09 +0100

    Implemented MouseObjectSway script to allow players to move slightly as the player pans their mouse

---
+++ HiJack/Assets/Scripts/Player/MouseObjectSway.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MouseObjectSway : MonoBehaviour
+{
+
+    public Vector2 amount;
+    public float lerp = .5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles = new Vector3(Mathf.LerpAngle(transform.localEulerAngles.x, y * amount.y, lerp), Mathf.LerpAngle(transform.localEulerAngles.y, x * amount.x, lerp), 0);
+        
+    }
+}
